12a06d54145d4ab45acdbbb7ce0f13fc727fa39f
g1nus <giuliano.andronic@studenti.unitn.it>
2018-05-04 21:50:04 +0200

    manages args and input

--- shell001.c
+++ shell001.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-#include<stdlib.h> 
-
-#include "arg_mngr.h"//includo l'agrs_manager
+#include<stdlib.h>
 
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
@@ -10,6 +8,8 @@
 #define BLUE    "\x1b[34m" //colori carini
 #define MAGENTA "\x1b[35m"
 #define RESET   "\x1b[0m"
+
+#include "big_mngr.h"//includo l'agrs_manager
 
 int main(int argc, char *argv[]){
     char *err_path, *out_path; int max_len=-1, code=-1; err_path=NULL; out_path=NULL;//conterranno i contenuti delle opzioni che verranno usati
@@ -23,8 +23,10 @@ int main(int argc, char *argv[]){
     char *input_buffer;//buffer effettivo
     size_t buff_size = 100;//numero di caratteri che il buffer puo' contenere
     /*--> variabili necessarie per fare i token */
-    char *tok;//conterra' i token
+    char *arr[10];//conterra' il vettore delle stringe passate(attualmente puo' contenere 10)
+    char *cmd[10];//conterra' il vettore dei comandi passati(puo' essere composto da piu' strighe)
     int x;//conterra' il numero di token trovati
+    int y;//conterra' i comandi suddivisi in maniera leggibile
     /*--> creo buffer per l'input */
     input_buffer = (char *)malloc(buff_size * sizeof(char));//alloco la memoria necessaria per il buffer
     if(input_buffer == NULL){//controlla che il buffer sia stato effettivamente creato
@@ -33,18 +35,20 @@ int main(int argc, char *argv[]){
     }
     /*--> prendo in input i comandi */
     while(strcmp(input_buffer, "quit")){//continuo finche' l'utente non inserisce quit
-        x = 0;//contatore per il presunto numero di comandi trovati(token)
-        printf("shell> ");
+        printf(RESET "shell> ");
         scanf ("%[^\n]%*c", input_buffer);//metto in input_buffer la linea presa in input(scanf legge fino a quando non trova invio)
-        printf(BLUE "<info> string: %s - n_char : %i \n", input_buffer, strlen(input_buffer));
+        printf(BLUE "<main:info> string: %s - n_char : %i \n", input_buffer, strlen(input_buffer));
         /*--> divisione della stringa in tokens */
-        tok = strtok(input_buffer, "|");//tok conterra' in questo primo caso tutto cio' che c'e' prima del primo pipe
-        while(tok != NULL){
-            printf("<info> tok[%i] : %s \n", x, tok);
-            x++;
-            tok = strtok(NULL, "|");//continuo con la restante stringa fino al prossimo pipe o fino alla fine
+        tok_manager(input_buffer, &arr, &cmd, &x, &y);
+        printf("<main:info> total arguments received  : %i --> ", x);
+        for(int i=0; i<x; i++){
+            printf("(%s) ", arr[i]);
         }
-        printf("<info> n_tokens : %i \n" RESET, x);
+        printf("\n<main:info> total commands received : %i --> ", y);
+        for(int i=0; i<y; i++){
+            printf("(%s) ", cmd[i]);
+        }
+        printf("\n");
     }
     return 0;
 }
