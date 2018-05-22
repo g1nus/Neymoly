16f08a267a8d19a3ff27d185b0387e00631ad87f
g1nus <giuliano.andronic@studenti.unitn.it>
2018-05-22 17:53:16 +0200

    cerca in system fino all'ultimo figlio

--- src/last_pid.sh
+++ src/last_pid.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cpids=$1
 
-#while [ -n "$cpids" ]
-#do
+while [ -n "$cpids" ]
+do
+    last_pid=$cpids
     #echo "child of $cpids is"
-    spids=`pgrep -P $cpids`
-    #echo "$cpids"
-#done
+    cpids=`pgrep -P $cpids`
+done
 #echo "last pid is $last_pid"
 #kill $last_pid
 #echo "the son of $1 is $spids, gonna kill it"
-kill $spids
+kill $last_pid
 echo "terminated process beacuse of timeout"
