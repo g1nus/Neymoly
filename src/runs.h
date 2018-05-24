b8e81b65f2a6eafbf2576c54861e926e1b915d4e
g1nus <giuliano.andronic@studenti.unitn.it>
2018-05-24 18:24:24 +0200

    pulito pezzi di codice

--- src/runs.h
+++ src/runs.h
@@ -1,221 +1,195 @@
-int cont=1;
+int going;//variabile usata dalla solo_run per notificare quando il figlio smette di eseguire
 
 char klr[500];//conterra' il path allo script che si occupa di terminare il figlio che si genera con la chiamata system
 
 void terminate_handler(){
-  cont=0;
+    // -> se viene ricevuto un segnale da parte del figlio verra' messo going a 0 perche' vuol dire che non sta piu' eseguendo
+    going=0;
 }
 
-/*void k_handler(){
-    system("clear");
-    fflush(stdin);
-    remove("/tmp/tmpout.cmd");
-    exit(0);
-}*/
-
-//metodo che controlla se il comando è cd e se può lo esegue
-int isCD(char *command)
-{
-    int i=0; //la posizione di "cd" nel comando
-    int j;
-    int res=-1; //controllo cosa so (se è 1 ho trovato cd)
+int isCD(char *command){
+    // -> metodo che controlla se il comando e' cd e se puo' lo esegue
+    int i=0;//conterra' la posizione di "cd" nel comando
+    int j;//servira' a scorrere fino alla fine del path
+    int res=-1;//se sara' 1 vuol dire che ho trovato cd
     int len=strlen(command);
-    char path[1024];
-    while((res==-1)&&(len>i+4)) //controllo che non abbia già trovato cd e che ci sia spazio rimanente per un path
-    {
-        if(command[i]==' ')
+    char path[100];//conterra' il path a cui si vuole accedere
+    while((res == -1) && (len > i + 4)){//controllo che non abbia già trovato cd e che ci sia spazio rimanente per un path
+        if(command[i] == ' '){//se trovo spazio scorro in avanti di uno
             i++;
-        else if ((command[i]=='c')&&(command[i+1]=='d')&&(command[i+2]==' '))
+        }else if ((command[i] == 'c') && (command[i+1] == 'd') && (command[i+2] == ' ')){//metto res a 1 se trovo "cd "
             res=1;
-        else
+        }else{//altrimenti rimane a 0
             res=0;
+        }
     }
-    if(res==1)//se è il comando cd entra qui
-    {
-	printf("<isCD:info> ho trovato il comando cd\n");
-        //aumento i fino ad togliere tutti i parametri
-        i+=3; //per arrivare alla prima parola dopo lo spazio
-        int findparameter=0;    //se vale 1 sto passando sopra un parametro
-        while(((command[i]==' ')||(command[i]!=' ' && findparameter==1)||(command[i]=='-'))&&(i<len))
-        {
-            if(command[i]=='-')
+    if(res == 1){//se ho trovato il comando cd provo a cercare il path
+        i += 3;//per arrivare alla prima parola dopo lo spazio
+        int findparameter=0;//se vale 1 sto passando sopra un parametro
+        while(((command[i] == ' ') || (command[i] != ' ' && findparameter == 1) || (command[i] == '-')) && (i < len)){//ciclo sui possibili parametri
+            if(command[i] == '-'){
                 findparameter=1;
-            else if(findparameter==1 && command[i]==' ')
+            }else if(findparameter==1 && command[i]==' '){
                 findparameter=0;
+            }
             i++;
         }
-        //a questo punto i dovrebbe essere la prima lettera del path
-        if(i<len)    //verifico che si possa effettivamente continuare
-        {
+        /*a questo punto l'indice i dovrebbe essere la prima lettera del path*/
+        if(i<len){//verifico che si possa effettivamente continuare
             j=i;
-            while(command[j]!=' ' && j<len){
+            while(command[j] != ' ' && j < len){//scorro con j sul presunto path
                 j++;
             }
-            printf("<isCD:info> last character of folder is %c\n", command[j-1]);
-            //strncpy(path, command+i, j-1);
-            memcpy( path, &command[i], j);
-            strtok(path," ");
-            printf("<isCD:info> path is: (%s)\n", path);
-            findparameter=0;
-            for(;j<len;j++){
-                if(command[j]!=' '){
-                    printf("<isCD:error> there's something fishy going on here\n");
-                    findparameter=1;
+            memcpy(path, &command[i], j);//mi salvo il path che inizia in i e finisce in j
+            strtok(path, " ");//tolgo lo spazio dal path se presente
+            findparameter = 0;
+            for(; j < len; j++){//controllo che non ci sia qualcos'altro dopo il path
+                if(command[j] != ' '){//se c'e' qualcos'altro setto findparameter a 1
+                    findparameter = 1;
+                    res = 0;
+                }
+            }
+            if(findparameter == 0){//se non c'e' altro provo a fare chdir
+                if(chdir(path) < 0){//controlla se ha successo nel cambiare cartella
+                    fprintf(stderr, RED "couldn't access directory\n" RESET);
                     res=0;
                 }
             }
-            if(findparameter==0){
-                if(chdir(path)<0){ //controlla se ha successo nel cambiare cartella
-                    printf("<isCD:error> couldn't access directory\n");
-                    res=0;
-                }
-            }
-            char cwd[1024];
-            if (getcwd(cwd, sizeof(cwd)) != NULL)
-                printf("<isCD:info> cartella di lavoro dopo il comando:: %s\n", cwd);
         }
     }
-    printf("<isCD:info> i=%i len=%i\n", i, len);
-    return res;
+    return res;//ritorno l'outcome della funzione
 }
 
 void solo_run(char *command, char *out_path, char *err_path, int max_len, int code, int timeout, int input, int output, int error, int cc, int numd, int isGT){
-    /*contiene comando, path per il log di output, path per il log di errore, lughezza massima dell'output, flag per il codice di ritorno,
-    canali di fd, output ed errore, codice del comando, codice del sottocomando e controllo per la gestione dei file*/
-    int cd = isCD(command);
-    printf("<solo_run:info>cd = %i\n", cd);
-    int fd;//uno per il file di log di output  e uno per il file di log di errori
+    // - solo_run si occupa di eseguire i comandi, gli vengono passati: comando, path per il log di output, path per il log di errore, lughezza massima dell'output, flag per il codice di ritorno, il tempo di timeout, canali di input, output ed errore, ID del comando, ID del sottocomando e flag controllo per la gestione dei file
+    int cd = isCD(command);//prima di tutto controllo se si tratta di un "cd" e se necessario cambio la working directory
+
+    /*dichiaro i file descriptors necessari*/
+    int fd;//conterra' il file descriptor per il file di log e output
     int tmp_out;//conterra' temporaneamente l'output del programma(file)
     int tmp_err;//conterra' temporaneamente lo stderr del programma(file)
     int tmp_date;//conterra' temporaneamente la data di esecuzione del programma(file)
-    int tmp_ret;//conterra' il codice di ritorno del programma
-    int r=0;//conterra' il valore di ritorno della funzione
-    size_t wbytes;//conterra' il numero di byte scritti dal programma
-    char *cmd_strout, *cmd_strerr, *cmd_strdate, cmd_strr[10];//conterra' le stringhe restituite dal programma
-    char tmp[12]={0x0};
-    int t=0;//indica il numero delle letture
-    int timeoutx=2;
-    int pid, ppid,pppid=getpid(), status;
-    char killer[300];char victim[50];
-    //killer=(char *)malloc(30 * sizeof(char));
-    strcpy(killer, "./last_pid.sh ");
+    int tmp_ret;//conterra' il codice di ritorno del programma(file)
+    int r = 0;//conterra' il valore di ritorno del comando sotto forma di intero
 
-    //* --> preparo i file per l'esecuzione dei comandi sul file temporaneo */
-    dup2(standard_out, 1);
-    if(isGT==0){
+    /*dichiaro ulteriori variabili di supporto*/
+    size_t wbytes;//conterra' il numero di byte scritti nel file dal programma
+    char *cmd_strout, *cmd_strerr, *cmd_strdate, cmd_strr[10];//conterranno le stringhe restituite dal programma, la data e il codice di ritorno
+    int t=0;//indichera' quante volte e' stato letto l'output del programma, serve principalmente per identificare i comandi bloccanti e fare timeout
+    int pid, ppid = getpid();//conterrano il pid del figlio e l'attuale pid(padre)
+    char tmp[12] = {0x0};//carattere di tabulazione
+    char killer[300];char victim[50];//conterrano la stringa per lanciare lo script che termina il figlio e il pid del figlio sotto forma di stringa
+
+    /*preparo i file necessari all'output del comando*/
+    if(isGT == 0){//se non si tratta di dover leggere da un file che contiene un output precedente
         remove("/tmp/tmpout.cmd");//rimuove l'output del precedente comando se presente
         remove("/tmp/tmperr.cmd");//rimuove l'output del precedente comando se presente
         remove("/tmp/tmpdate.cmd");//rimuove la date del precedente comando se presente
         remove("/tmp/tmprrr.cmd");//rimuove la date del precedente comando se presente
-        printf(YELLOW "[%i]<solo_run:info> command to run is (%s), cc=%i, numd=%i\n" RESET,getpid(), command, cc, numd);
-    }else{
-        printf(YELLOW "[%i]<solo_run:info> this is a file: %s\n, cc=%i, numd=%i\n" RESET,getpid(), command, cc, numd);
     }
     tmp_date=open("/tmp/tmpdate.cmd", O_RDWR | O_CREAT, 0777);//apro il file in cui verra' salvato temporaneamente la data del programma
-    tmp_err=open("/tmp/tmperr.cmd", O_RDWR | O_CREAT, 0777);//apro il file in cui verra' salvato temporaneamente lo stderr del programma
-    tmp_out=open("/tmp/tmpout.cmd", O_RDWR | O_CREAT, 0777);//apro il file in cui verra' salvato temporaneamente lo stdout del programma
+    tmp_err=open("/tmp/tmperr.cmd", O_RDWR | O_CREAT, 0777);//apro il file in cui verra' salvato lo stderr del programma
+    tmp_out=open("/tmp/tmpout.cmd", O_RDWR | O_CREAT, 0777);//apro il file in cui verra' salvato lo stdout del programma
     tmp_ret=open("/tmp/tmprrr.cmd", O_RDWR | O_CREAT, 0777);//apro il file in cui verra' salvato il codice di ritorno
 
-
-
-    fflush(stdout);
-    //* --> eseguo la data su un file temporaneo per poi salvarmele */
+    /*eseguo la data sul file di data temporaneo per poi salvarmela*/
     dup2(tmp_date,1);//fa in modo che lo stdout punti al file che conterra' la data di esecuzione
     system("date");//eseguo il comando che mi da la data
-
     fflush(stdout);
 
-    //* --> eseguo il comando passato sui file temporanei e poi rimetto stout e stderr a quelli necessari */
-    dup2(input, 0);//fa in modo che l'fd derivi da tastiera o dal pipe passato
-    if(cd!=1 && isGT==0){//se il comando non e' un cd, oppure se e' un cd sbagliato
-        if ((pid = fork()) == 0){//son
-
-            //signal(SIGKILL, k_handler);
-            dup2(tmp_out,1);
-            if(strcmp(command,"nano")!=0 && strcmp(command,"pico")!=0){
-                dup2(tmp_err,2);
+    /*eseguo il comando se necessario*/
+    dup2(input, 0);//fa in modo che l'input derivi da tastiera o dal pipe passato alla funzione come input
+    if(cd != 1 && isGT == 0){//se si tratta di un comando valido forko
+        if ((pid = fork()) == 0){
+            /*figlio che si occupera' di eseguire il comando e di stampare il suo output nei file temporanei*/
+            dup2(tmp_out,1);//metto il stdout al file temporaneo
+            if(strcmp(command,"nano")!=0 && strcmp(command,"pico")!=0){//fix per nano e pico che non accettano tmp_err(?)
+                dup2(tmp_err,2);//metto il stderr al file temporaneo
             }
-            r=WEXITSTATUS(system(command));fflush(stdout);fflush(stderr);
-            close(tmp_err);
+            r=WEXITSTATUS(system(command));fflush(stdout);fflush(stderr);//eseguo il comando grazie a system, WEXITSTATUS mi ritornera' il codice di ritorno dell'esecuzione
+            close(tmp_err);//finita l'esecuzione chiudo i file temporanei
             close(tmp_out);
-            //* --> converto il valore di ritorno in una stringa */
-            sprintf(cmd_strr, "%d", r);
+            sprintf(cmd_strr, "%d", r);//converto in una stringa il codice di ritorno per poterla stampare in un file
             write(tmp_ret,cmd_strr,strlen(cmd_strr));
-            kill(pppid,SIGUSR1);
-            exit(0);
-          }else{//father
-            int first_time=1;
-            cont=1;
-            sprintf(victim, "%i", pid);
-
-            signal(SIGUSR1, terminate_handler);
-            dup2(output,1);
-            //printf("waiting for magic to happen\n");fflush(stdout);
-            sleep(1.3);
-            while(cont || first_time){
-                if(!first_time){
-                    system("clear");
-                }else{
+            kill(ppid,SIGUSR1);//invio il segnale a padre per dire che ho finito
+            exit(0);//esco
+          }else{
+            /*padre che si occupera' di leggere cosa scrive il filgio nel file temporaneo e di inoltrarlo ai canali di output ed errore*/
+            signal(SIGUSR1, terminate_handler);//assegno al segnale l'esecuzione delle funzione terminate_handler
+            int first_time=1;//inzialmente setto first_time a 1 per specificare che sot facendo la prima lettura
+            going=1;//going a 1 vuol dire che il figlio sta teoricamente eseguendo
+            sprintf(victim, "%i", pid);//trasformo il pid del figlio in uns stringa
+            dup2(output,1);//setto l'output dove necessario
+            sleep(1.3);//aspetto che il figlio esegua almeno una volta
+            while(going || first_time){//se si tratta di un first time oppure se il figlio sta ancora eseguendo ciclo
+                if(first_time){//se era un first time ora non lo e' piu'
                     first_time=0;
                 }
-
-                wbytes = lseek(tmp_out, (size_t)0, SEEK_END);//mi salvo il numero di byte contenuti nel file di data
-                lseek (tmp_out, (off_t) 0, SEEK_SET);//riporto il puntatore all'inizio
-                //printf("found %i bytes to read\n",wbytes);
-                if((int) wbytes > 0){
-                    cmd_strout=(char *)malloc(wbytes * sizeof(char));//alloco lo spazio di memoria necessaria per salvarmi la data in una stringa
+                /*controllo l'output del figlio e lo stampo dove necessario*/
+                wbytes = lseek(tmp_out, (size_t)0, SEEK_END);//mi salvo il numero di byte contenuti nel file di output del figlio
+                lseek(tmp_out, (off_t)0, SEEK_SET);//riporto il puntatore all'inizio
+                //(might be useful) printf("found %i bytes to read\n",wbytes);
+                if((int)wbytes > 0){//se ho trovato dei bytes da leggere nel file di output
+                    cmd_strout=(char *)malloc(wbytes * sizeof(char));//alloco lo spazio di memoria necessario
                     if(cmd_strout == NULL){//controlla che il buffer sia stato effettivamente creato
                         fprintf(stderr, "<solo_run:error> failed to create buffer\n");
                         exit(1);
                     }
-                        if(read(tmp_out, cmd_strout, wbytes)<0){//salvo il contenuto del file nella stringa creata
-                        printf("<solo_run:error> there was an error reading the out file\n");
+                    if(read(tmp_out, cmd_strout, wbytes)<0){//salvo il contenuto del file nella stringa allocata
+                        fprintf(stderr, "<solo_run:error> there was an error reading the out file\n");
+                        exit(1);
                     }
-                    if(strlen(cmd_strout)!=wbytes){
+                    if(strlen(cmd_strout)!=wbytes){//fix per caratteri di fine stringa
                         cmd_strout[wbytes] = 00;
                     }
                     printf("%s", cmd_strout);//stampo il contenuto della stringa sullo schermo o nel pipe
                     fflush(stdout);
                 }
-
-                wbytes = lseek(tmp_err, (size_t)0, SEEK_END);//mi salvo il numero di byte contenuti nel file di data
-                lseek (tmp_err, (off_t) 0, SEEK_SET);//riporto il puntatore all'inizio
-                //printf("found %i bytes to read\n",wbytes);
-                if((int) wbytes > 0){
-                    cmd_strerr=(char *)malloc(wbytes * sizeof(char));//alloco lo spazio di memoria necessaria per salvarmi la data in una stringa
-                    if(cmd_strerr == NULL){//controlla che il buffer sia stato effettivamente creato
+                /*controllo lo stderr del figlio e lo stampo sempre a video stavolta*/
+                wbytes = lseek(tmp_err, (size_t)0, SEEK_END);
+                lseek (tmp_err, (off_t)0, SEEK_SET);
+                //(might be useful) printf("found %i bytes to read\n",wbytes);
+                if((int)wbytes > 0){
+                    cmd_strerr=(char *)malloc(wbytes * sizeof(char));
+                    if(cmd_strerr == NULL){
                         fprintf(stderr, "<solo_run:error> failed to create buffer\n");
                         exit(1);
                     }
-                        if(read(tmp_err, cmd_strerr, wbytes)<0){//salvo il contenuto del file nella stringa creata
-                        printf("<solo_run:error> there was an error reading the out file\n");
+                    if(read(tmp_err, cmd_strerr, wbytes)<0){
+                        fprintf(stderr, "<solo_run:error> there was an error reading the out file\n");
+                        exit(1);
                     }
                     if(strlen(cmd_strerr)!=wbytes){
                         cmd_strerr[wbytes] = 00;
                     }
-                    fprintf(stderr, "%s", cmd_strerr);//stampo il contenuto della string sullo schermo o nel pipe
+                    fprintf(stderr, "%s", cmd_strerr);//stampo il contenuto della stringa sullo schermo
                     fflush(stderr);
                 }
-                if(cont){
-                    sleep(1);
-                    system("clear");
+                if(going){//se il figlio sta ancora eseguendo
+                    sleep(1);//aspetto un secondo
+                    system("clear");//"pulisco"
                 }
                 t++;//incrementa il numero di letture
-                if(timeout>0 && (t>timeout && cont==1)){
-
-                    strcpy(killer,klr);
+                if(timeout>0 && (t>timeout && going==1)){//se e' stato definito un timeout e il figlio lo sta superando
+                    strcpy(killer,klr);//preparo la stringa per richiamare lo script
                     strcat(killer,victim);
-                    system(killer);
+                    system(killer);//richiamo lo script
                     sleep(1);
-                    //kill(pid,SIGQUIT);
                 }
             }
             close(tmp_out);
             close(tmp_err);
             fflush(stdout);
             fflush(stderr);
-            wait(&status);
+            wait(NULL);
           }
+
+
+
+
+
+
+
     }else if(cd==1){
         printf(" \n");
     }else if(isGT==1){
